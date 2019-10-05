309de803c4fcafb95d6c576e9040c4f8be2fca48
Dario Carella <dario.kar@gmail.com>
2019-10-05 12:12:06 +0200

    Update event interface

--- example/example.js
+++ example/example.js
@@ -1,10 +1,28 @@
-import Morbido from '../es/index';
+import 'regenerator-runtime/runtime';
+import Morbido from '../src/index';
 
-const changeButton = document.getElementById('change-button');
 const morbidoTarget = document.getElementById('morbido-target');
 const changingParagraph = document.getElementById('changing-paragraph');
 
-const morbido = new Morbido(morbidoTarget);
+const morbido = new Morbido(morbidoTarget, {
+  onExit: ({ mutation, exitingElement, enteringElement }) => {
+    console.log('[onExit]', mutation);
+    exitingElement.classList.add('hide');
+    enteringElement.classList.add('hide');
+
+    return new Promise(resolve => setTimeout(resolve, 600));
+  },
+  onMutation: ({ mutation }) => {
+    console.log('[onMutation]', mutation);
+    return new Promise(resolve => setTimeout(resolve, 1200));
+  },
+  onEnter: ({ mutation, enteringElement }) => {
+    console.log('[onEnter]', mutation);
+    enteringElement.classList.remove('hide');
+    return new Promise(resolve => setTimeout(resolve, 600));
+  },
+});
+morbido.watch();
 
 const DUMMY_PARAGRAPH =
   'Forte, frittata tortellini paparazzi caprese, forte, cupola zucchini\
@@ -13,10 +31,12 @@ tortellini spaghetti espresso, ciao panini pepperoni ballerina\
 zucchini gnocchi pepperoni, barista gnocchi mamma salami frittata\
 pepperoni.';
 
-changeButton.addEventListener('click', () => {
-  let pCount = 1 + Math.round(Math.random() * 2);
+let pCount = 0;
+setInterval(() => {
+  pCount = (pCount + 1) % 3;
+  let i = pCount + 1;
   let p = '';
-  while (pCount-- > 0) p += DUMMY_PARAGRAPH;
+  while (i-- > 0) p += ' ' + DUMMY_PARAGRAPH;
 
   changingParagraph.innerText = p;
-});
+}, 3000);
--- src/index.js
+++ src/index.js
@@ -1,36 +1,151 @@
+import delay from './utils/delay';
+import tick from './utils/tick';
+
 const defaultOptions = {
   watchAttributes: true,
+  watchCharacterData: true,
   watchChildList: true,
   watchSubtree: true,
+  timeout: {
+    enter: 0,
+    mutation: 0,
+    exit: 0,
+  },
+  onExit: f => f,
+  onMutation: f => f,
+  onEnter: f => f,
 };
 
 export default class Morbido {
-  constructor(el, options) {
-    this._target = el;
+  constructor(el, { timeout, ...options }) {
     this._options = { ...defaultOptions, ...options };
-    this._isWatching = false;
+    this._options.timeout = this._parseTimeouts(timeout || 0);
 
-    // wrap target
-    this._wrapper = this._buildWrapper();
-    this._target.parentNode.insertBefore(this._wrapper, this._target);
-    this._wrapper.appendChild(this._target);
+    this._target = el;
+    this._wrapper = this._target.parentNode;
+
+    this._isWatching = false;
+    this._mutationObserver = new MutationObserver(this._handleMutation);
+    this._isMutating = false;
   }
 
   // Private Methods
-  _buildWrapper() {
-    const div = document.createElement('div');
-    div.classList.add('morbido');
-    return div;
+  _parseTimeouts(timeout) {
+    let exit, mutation, enter;
+
+    exit = mutation = enter = timeout;
+
+    if (timeout != null && typeof timeout !== 'number') {
+      exit = timeout.exit;
+      mutation = timeout.mutation !== undefined ? timeout.mutation : exit;
+      enter = timeout.enter !== undefined ? timeout.enter : exit;
+    }
+    return { exit, mutation, enter };
   }
+  _getObserverOptions() {
+    return {
+      attributes: this._options.watchAttributes,
+      attributeOldValue: this._options.watchAttributes,
+      characterData: this._options.watchCharacterData,
+      characterDataOldValue: this._options.watchCharacterData,
+      childList: this._options.watchChildList,
+      subtree: this._options.watchSubtree,
+    };
+  }
+  _saveCurrentState() {
+    this._storedTarget = this._target.cloneNode(true);
+    this._storedSize = {
+      width: this._target.offsetWidth,
+      height: this._target.offsetHeight,
+    };
+  }
+  _handleMutation = async mutationsList => {
+    this._mutationObserver.disconnect();
+
+    this._wrapper = this._target.parentNode;
+
+    this._mutation = {
+      width: {
+        from: this._storedSize.width,
+        to: this._target.offsetWidth,
+      },
+      height: {
+        from: this._storedSize.height,
+        to: this._target.offsetHeight,
+      },
+    };
+
+    const storedTargetInlineTransition = this._storedTarget.style.transition;
+
+    // set size and no-transition inline to previous state element before attaching to dom
+    this._storedTarget.style.height = `${this._mutation.height.from}px`;
+    this._storedTarget.style.width = `${this._mutation.width.from}px`;
+    this._storedTarget.style.transition = `none`;
+
+    // replace the target with stored state (previous state) element
+    this._wrapper.replaceChild(this._storedTarget, this._target);
+
+    // keep transition disabled for a tick
+    await tick();
+    // then restore it
+    this._storedTarget.style.transition = storedTargetInlineTransition;
+
+    // wait for exit to happen
+    await Promise.all([
+      this._options.onExit({
+        mutation: this._mutation,
+        exitingElement: this._storedTarget,
+        enteringElement: this._target,
+      }),
+      delay(this._options.timeout.exit),
+    ]);
+
+    // set new size inline to stored target (attached to dom)
+    this._storedTarget.style.height = `${this._mutation.height.to}px`;
+    this._storedTarget.style.width = `${this._mutation.width.to}px`;
+
+    // wait for mutation to happen
+    await Promise.all([
+      this._options.onMutation({
+        mutation: this._mutation,
+        exitingElement: this._storedTarget,
+        enteringElement: this._target,
+      }),
+      delay(this._options.timeout.mutation),
+    ]);
+
+    // replace the stored state (previous state) with target
+    this._wrapper.replaceChild(this._target, this._storedTarget);
+
+    await tick();
+
+    // wait for enter to happen
+    await Promise.all([
+      this._options.onEnter({
+        mutation: this._mutation,
+        exitingElement: this._storedTarget,
+        enteringElement: this._target,
+      }),
+      delay(this._options.timeout.enter),
+    ]);
+
+    this._saveCurrentState();
+
+    this._mutationObserver.observe(this._target, this._getObserverOptions());
+  };
 
   // Public Methods
-  watch(count) {
+  watch() {
     if (this._isWatching) {
       return false;
     }
 
-    this._watchingCount = count;
     this._isWatching = true;
+
+    this._saveCurrentState();
+
+    // Start observing the target node for configured mutations
+    this._mutationObserver.observe(this._target, this._getObserverOptions());
 
     return true;
   }
@@ -39,6 +154,9 @@ export default class Morbido {
     if (!this._isWatching) {
       return false;
     }
+
+    // Stop observing
+    this._mutationObserver.disconnect();
 
     this._isWatching = false;
 
